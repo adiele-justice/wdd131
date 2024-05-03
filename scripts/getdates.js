28e363fd2789086c74ec190c9fa6623426123643
adiele-justice <adi24004@byui.edu>
2024-05-03 20:44:15 +0100

    week01

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// scripts/getdates.js
+
+// Dynamically populate current year
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Dynamically populate last modified date
+document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
