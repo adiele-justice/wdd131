ddee44c1685a26933e83358f428e0eb63236dfbd
adiele-justice <adi24004@byui.edu>
2024-05-30 02:53:49 +0100

    week04

--- scripts/temples.js
+++
@@ -1,8 +0,0 @@
-// scripts/getdates.js
-
-// Dynamically populate current year
-const currentYear = new Date().getFullYear();
-document.getElementById('currentyear').textContent = currentYear;
-
-// Dynamically populate last modified date
-document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
